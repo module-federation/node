bc75e8d8611134ff70b6bb029b77bbcca9c3ce90
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-09-13 11:38:45 -0700

    move nextjs-mf over to node repo

---
+++ index.js
@@ -0,0 +1,5 @@
+const NodeTargetPlugin = require('./src/NodeFederationPlugin');
+const StreamingTargetPlugin = require('./src/StreamingTargetPlugin');
+
+module.exports.NodeTargetPlugin = NodeTargetPlugin;
+module.exports.StreamingTargetPlugin = StreamingTargetPlugin;
