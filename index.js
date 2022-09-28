e55168de12f54d4fa9e91ea7daf2fa99d7ac21ce
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-09-27 19:46:51 -0700

    wipe repo

--- index.js
+++
@@ -1,7 +0,0 @@
-const NodeFederationPlugin = require('./src/NodeFederationPlugin');
-const StreamingTargetPlugin = require('./src/StreamingTargetPlugin');
-const UniversalFederationPlugin = require('./src/UniversalFederationPlugin');
-
-module.exports.NodeFederationPlugin = NodeFederationPlugin;
-module.exports.StreamingTargetPlugin = StreamingTargetPlugin;
-module.exports.UniversalFederationPlugin = UniversalFederationPlugin;
